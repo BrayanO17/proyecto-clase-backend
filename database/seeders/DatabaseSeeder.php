97a52013c6085474aaacdddc187235c66191e91f
Brayan Olaya <bolaya@unab.edu.co>
2026-03-11 19:21:53 -0500

    Creación de Factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Product;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -10,15 +12,12 @@ class DatabaseSeeder extends Seeder
 {
     use WithoutModelEvents;
 
-    /**
-     * Seed the application's database.
-     */
+
     public function run(): void
     {
-        // User::factory(10)->create();
 
-        $this->call([
-            CategorySeeder::class
-        ]);
+
+        Category::factory(200)->create();
+        Product::factory(200)->create();
     }
 }
